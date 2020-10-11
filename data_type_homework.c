410757772a1091dc765d51bfaf47cb88f027f0b4
davids <davidsliksnitis@gmail.com>
2020-10-12 02:09:51 +0300

    20201011md

---
+++ data_type_homework.c
@@ -0,0 +1,37 @@
+ #include<stdio.h>
+
+
+
+ void main()
+{
+
+ char a =15;
+
+
+ printf("Mainigais a unsigned decimal integer ir: %u\n",a);
+ printf("Mainigais a unsigned octal ir: %o\n",a);
+ printf("Mainigais a unsigned hexadecimal integer ir: %x\n",a);
+
+ double c = 4.3782475793;
+
+ printf("Mainigais c decimal floating point ir: %f\n",c);
+ printf("Mainigais c scientific notation ir: %e\n",c);
+ printf("Mainigais c shortest representation ir: %g\n",c);
+ printf("Mainigais c hexadecimal floating point ir: %a\n",c);
+
+ int d;
+
+ printf("Mainigais d character ir: %c\n",d);
+
+ unsigned int f = 32;
+
+ printf("Mainigais f unsigned decimal integer ir: %u\n",f);
+ printf("Mainigais f unsigned octal ir: %o\n",f);
+ printf("Mainigais f unsigned hexadecimal integer ir: %x\n",f);
+
+ unsigned int h = 85;
+
+ printf("Mainigais h unsigned decimal integer with spaces ir: %10u\n",h);
+ printf("Mainigais h unsigned decimal integer with zeros ir: %010u\n",h);
+
+}
