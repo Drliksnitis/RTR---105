73b9cdabc871ed9682902e1a1010b70d291533c0
Drliksnitis <71380679+Drliksnitis@users.noreply.github.com>
2020-10-25 21:32:37 +0200

    Update dec_to_bin.c

--- dec_to_bin.c
+++ dec_to_bin.c
@@ -22,36 +22,5 @@ int a, b, c;
   printf("\n");
 
 
-
-
-
-
-
-/*
-char a,b,c;
-
-printf("Ievadiet decimal skaitli: \n");
-scanf("%d",&a);
-
-b = 2;
-printf("%d % %d = %d\n",a,b,c);
-
-
-/*
-dn=n;
-i=1;
-
- for(j=n;j>0;j=j/2)
- {
-  binno=binno+(n%2)*i;
-  i=i*10;
-  n=n/2;
-
- }
-
- printf("Binarais skaitlis ir %d skaitlim %d. \n",binno,dn);
-
-
-*/
 return 0;
 }
